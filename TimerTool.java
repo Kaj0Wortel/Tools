e2e2deea576a5ba15ab24a1e1d67cf8cf559ab8e
Kaj0Wortel <36736012+Kaj0Wortel@users.noreply.github.com>
2018-04-06 18:29:42 +0200

    Added comments and fixed layout

--- TimerTool.java
+++ TimerTool.java
@@ -10,7 +10,7 @@
  * It is not allowed to redistribute any (modifed) versions of this file     *
  * without my permission.                                                    *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-//todo
+
 package tools;
 
 
@@ -21,66 +21,67 @@ import java.util.concurrent.ExecutionException;
 
 
 public class TimerTool {
-    private Timer timer = new Timer(true);
-    private Runnable task; // the task to be executed. Called via rask.run()
-    private Long delay; // the initial delay
-    private Long interval; // the timerinterval
+    private Timer timer; // The current timer object.
+    final private Runnable[] tasks; // The tasks to be executed.
+    private Long delay; // The initial delay
+    private long interval; // The timerinterval
     
-    // the start timestamp of the timer for the current iteration
-    private Long startTime = System.currentTimeMillis();
+    // The start timestamp of the timer for the current iteration
+    private Long startTime;
     
-    // the pause timestamp of the timer. If there was no pause in
+    // The pause timestamp of the timer. If there was no pause in
     // this iteration then it is equal to the start timestamp
-    private Long pauseTime = System.currentTimeMillis();
+    private Long pauseTime;
     
-    // the current state of the timer
+    // The current state of the timer
     private TimerState timerState = TimerState.CANCELED;
     public enum TimerState {
         RUNNING, PAUSED, CANCELED
     }
     
-    // --------------------------------------------------------------------------------------------------------
-    // Constructor
-    public TimerTool(Runnable r, Long delay, Long interval) {
+    /* --------------------------------------------------------------------------------------------------------
+     * Constructor
+     * --------------------------------------------------------------------------------------------------------
+     */
+    /*
+     * @param r the action that will be executed when the timer ends.
+     * @param delay the time in ms before the first exectution of {@code r.run()}.
+     * @param interval the time in ms which is between two executions of {@code r.run()}.
+     */
+    public TimerTool(Long delay, long interval, Runnable... rs) {
         // Update the values to the values in this class
-        this.task = r;
+        this.tasks = rs;
         this.delay = delay;
         this.interval = interval;
         
-        // Initialize the timer. Also checks if the interval is not equal to 'null'
-        // If so, set the interval to 10 ms
-        // NOTE that this.interval still equals null!
-        if (interval != null) {
-            timer = new Timer(true);
-        
-        } else {
-            timer = new Timer(false);
-        }
+        // Create new timer.
+        timer = new Timer(true);
         
         // Checks if the delay is not equal to 'null'.
-        // If so, set the initial deylay equal to '0'
+        // If so, set the initial delay to '0'
         if (delay == null) {
             delay = 0L;
-            this.delay = delay;
         }
+        
+        this.delay = delay;
         
         // For the first iteration is the start time modified because then there are no problems
         // with the pause/resume functions if the timer is still in the initial delay.
-        if (interval != null) {
-            startTime = System.currentTimeMillis() + delay  - interval;
-        } else {
-            startTime = System.currentTimeMillis() + delay;
-        }
+        startTime = System.currentTimeMillis() + delay - interval;
     }
     
-    // --------------------------------------------------------------------------------------------------------
-    // Functions
     
+    /* --------------------------------------------------------------------------------------------------------
+     * Functions
+     * --------------------------------------------------------------------------------------------------------
+     */
     /* 
      * Create a new timer task from the given runnable.
      * Also updates the start time and the pause time.
+     * 
+     * @param rs the tasks to be executed. Is allowed to be null, but this is not effective.
      */
-    private TimerTask createTimerTask(Runnable r) {
+    private TimerTask createTimerTask(Runnable... rs) {
         return new TimerTask() {
             @Override
             public void run() {
@@ -88,18 +89,19 @@ public class TimerTool {
                 startTime = System.currentTimeMillis();
                 pauseTime = System.currentTimeMillis();
                 
-                // Run the function
-                r.run();
-                
-                if (interval == null) {
-                    timer.cancel();
+                // Run the function(s)
+                if (rs != null) {
+                    for (Runnable r : rs) {
+                        r.run();
+                    }
                 }
             }
         };
     }
     
     /* 
-     * (Re)-starts the timer
+     * (Re)-starts the timer.
+     * If the timer is already running, purge the timer and create a new timer.
      */
     public void start() {
         if (timerState == TimerState.RUNNING) {
@@ -107,93 +109,114 @@ public class TimerTool {
             timer.purge();
         }
         
-        // update the timestamps
+        // Update the timestamps
         startTime = System.currentTimeMillis();
         pauseTime = System.currentTimeMillis();
         
-        timerState = TimerState.RUNNING;
         timer = new Timer(true);
         
-        if (interval != null) {
-            timer.scheduleAtFixedRate(createTimerTask(task), delay, interval);
-        } else {
-            timer.schedule(createTimerTask(task), delay);
-        }
+        timer.scheduleAtFixedRate(createTimerTask(tasks), delay, interval);
+        
+        // Update the timeState
+        timerState = TimerState.RUNNING;
     }
     
     /* 
-     * Pauses the timer
+     * Pauses the timer.
+     * If the timer is paused or stopped, no acion is taken.
      */
     public void pause() {
-        if (timerState == TimerState.RUNNING) {
-            timer.cancel();
-            timer.purge();
-        }
-        timerState = TimerState.PAUSED;
+        if (timerState == TimerState.PAUSED || timerState == TimerState.CANCELED) return;
+        
+        timer.cancel();
+        timer.purge();
+        
+        // Set the pause time stamp
         pauseTime = System.currentTimeMillis();
+        
+        // Update the timeState
+        timerState = TimerState.PAUSED;
     }
     
     /* 
      * Resumes a paused timer
+     * If the timer is running or canceled, no action is taken.
      */
     public void resume() {
-        if (timerState == TimerState.RUNNING) {
-            timer.cancel();
-            timer.purge();
-        }
-        long newDelay = interval - (pauseTime - startTime);
-        if (newDelay < 0) {
-            newDelay = 0;
-        }
+        if (timerState == TimerState.RUNNING || timerState == TimerState.CANCELED) return;
         
-        // update the timestamps
-        startTime = System.currentTimeMillis() - newDelay;
-        pauseTime = System.currentTimeMillis() - newDelay;
+        // The current time
+        long curTime = System.currentTimeMillis();
         
-        timerState = TimerState.RUNNING;
+        // Calculate the initial delay.
+        long timeBeforeRun = interval - (curTime - startTime);
+        long startDelay = (timeBeforeRun < 0 ? 0 : timeBeforeRun);
+        
+        // Update the start time stamp.
+        startTime = System.currentTimeMillis() - timeBeforeRun;
+        
         timer = new Timer(true);
         
-        if (interval != null) {
-            timer.scheduleAtFixedRate(createTimerTask(task), delay, interval);
-        } else {
-            timer.schedule(createTimerTask(task), delay);
-        }
+        timer.scheduleAtFixedRate(createTimerTask(tasks), startDelay, interval);
+        
+        // Update the timeState
+        timerState = TimerState.RUNNING;
     }
     
     /* 
-     * Cancels a scheduled timer
+     * Cancels a running or canceled timer.
      */
     public void cancel() {
+        // Kill the current timer.
         if (timerState == TimerState.RUNNING) {
             timer.cancel();
             timer.purge();
         }
+        
+        // Update the timeState
         timerState = TimerState.CANCELED;
     }
     
     /* 
-     * Sets a new interval for executing a task
+     * Sets a new interval for the timer.
+     * 
+     * @param interval the new interval to be set.
      */
     public void setInterval(long interval) {
+        // If the interval is equal, return immediately.
+        if (interval == this.interval) return;
+        
+        // Update the interval
         this.interval = interval;
         
+        // The current time
+        long curTime = System.currentTimeMillis();
+        
+        // If the timer is running, kill it.
         if (timerState == TimerState.RUNNING) {
             timer.cancel();
             timer.purge();
             
-            long newDelay = interval - (System.currentTimeMillis() - startTime);
-            if (newDelay < 0) {
-                newDelay = 0;
+            // Calculate the initial delay.
+            long timeBeforeRun = interval - (curTime - startTime);
+            long startDelay = (timeBeforeRun < 0 ? 0 : timeBeforeRun);
+            
+            // Update the start timestamp if the timer has to start directly.
+            if (timeBeforeRun < 0) {
+                startTime = curTime + timeBeforeRun;
             }
             
-            // update the timestamps
-            startTime = System.currentTimeMillis() - newDelay;
-            pauseTime = System.currentTimeMillis() - newDelay;
-            
+            // Start a new timer
             timer = new Timer(true);
-            timer.scheduleAtFixedRate(createTimerTask(task), newDelay, interval);
-            pauseTime = System.currentTimeMillis();
+            timer.scheduleAtFixedRate(createTimerTask(tasks), startDelay, interval);
         }
+    }
+    
+    /* 
+     * @return the current state of the timer.
+     */
+    public TimerState getState() {
+        return timerState;
     }
     
 }
