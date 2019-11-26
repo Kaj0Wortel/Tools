eb50c9ae874ee3c452bc91bc1d44d635261c8f1b
Kaj0Wortel <kaj.wortel@gmail.com>
2019-11-26 02:30:45 +0100

    Added red black tree implementation (2)

---
+++ src/tools/data/collection/rb_tree/RBKey.java
@@ -0,0 +1,30 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright (C) September 2019 by Kaj Wortel - all rights reserved          *
+ * Contact: kaj.wortel@gmail.com                                             *
+ *                                                                           *
+ * This file is part of the tools project, which can be found on github:     *
+ * https://github.com/Kaj0Wortel/tools                                       *
+ *                                                                           *
+ * It is allowed to use, (partially) copy and modify this file               *
+ * in any way for private use only by using this header.                     *
+ * It is not allowed to redistribute any (modified) versions of this file    *
+ * without my permission.                                                    *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+package tools.data.collection.rb_tree;
+
+
+/**
+ * Interface for classes to be used in red-black trees.
+ * 
+ * @author Kaj Wortel
+ */
+public interface RBKey {
+    
+    /**
+     * @return The value of the data object used as key in the red-black tree.
+     */
+    public int value();
+    
+    
+}
